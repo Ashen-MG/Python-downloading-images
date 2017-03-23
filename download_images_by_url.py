74abb88fc1eda3c84283ea6df7faf8450f047f82
MartysHD <martingergelhd@gmail.com>
2017-03-23 18:19:13 +0100

    Added Counting

--- download_images_by_url.py
+++ download_images_by_url.py
@@ -5,10 +5,11 @@ import os
 import urllib.request
 
 x = 1
+y = 1
+z = 1
 
-# Naming and downloading images
 
-
+#  Naming and downloading images
 def naming_downloading_images(path, file_format, source):
     global x
 
@@ -19,12 +20,12 @@ def naming_downloading_images(path, file_format, source):
         try:
             file_name = os.path.join(path + "/", name)
             urllib.request.urlretrieve(source, file_name)
-            print("Downloaded!")
+            print("Downloaded! \n\n")
             x += 1
         except:
             print("Some error issue.")
     except:
-        print("File doesn't exist anymore or it's invalid.")
+        print("File doesn't exist anymore or it's invalid. \n\n")
 
 
 try:
@@ -38,17 +39,27 @@ try:
     # FILE
     file_path = input("Put full path (key sensitive) where you want to save images (directory) Tip: Put / before: ")
 
+    # Check if file exists
     if os.path.exists(file_path) and os.path.isdir(file_path):
 
+        #  Counting how many images are going to be downloaded
+        for count_link in soup.findAll("img"):
+            count_src = count_link.get("src")
+            y += 1
+
+        # Crawling
         for link in soup.findAll("img"):
 
             src = link.get("src")
+
+            print("Downloading (" + str(z) + "/" + str(y) + ")")
 
             if str(url_parse_check[0]) not in src:
 
                 final_link = url_input + src
                 print(final_link)
 
+                # Saving as png or jpeg/jpg
                 if ".png" in src:
                     try:
                         naming_downloading_images(file_path, ".png", final_link)
@@ -71,6 +82,8 @@ try:
                     except:
                         continue
 
+            z += 1
+
     else:
         print("Path doesn't exist or place where you want to save images is not directory.")
 
@@ -78,4 +91,4 @@ except KeyboardInterrupt:
     print("")
 except:
     print("That url doesn't exist or you typed it wrong. Tip: Make sure you're copying it from website and watch "
-        "out for protocols (http, https)")
+          "out for protocols (http, https)")
